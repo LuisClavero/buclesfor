72cb4f0d4fdf58e6ca9f350cbd7c801d144cfc92
ism49186163 <ism49186163@a05.informatica.escoladeltreball.org>
2018-02-08 13:41:24 +0100

    Version actualizada

---
+++ for02.py
@@ -0,0 +1,14 @@
+#coding: utf-8
+num=input("Escriba un número:")
+div=2
+while num<0:
+	num=input("Le he pedido un numero mayor que cero, escriba otro:")
+for i in range(0,num):
+	while num%div==0:
+		print div,"es divisor de",num
+		div=div+1
+	else:
+		div=div+1
+print "Fin"
+
+
